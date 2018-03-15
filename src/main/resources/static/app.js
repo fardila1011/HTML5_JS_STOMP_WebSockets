b5dff8b55ed850e08059c02dc37060454497bf36
Fabian Ardila Rodriguez <fabianra97@gmail.com>
2018-03-14 23:45:57 -0500

    PARTE 2

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,38 +1,38 @@
 var app = (function () {
 
-    class Point{
-        constructor(x,y){
-            this.x=x;
-            this.y=y;
+    class Point {
+        constructor(x, y) {
+            this.x = x;
+            this.y = y;
         } 
-        
+
         x() {
             return this.x;
         }
-        
+
         y() {
             return this.y;
         }
     }
-    
+
     var stompClient = null;
 
-    var addPointToCanvas = function (point) {        
+    var addPointToCanvas = function (point) {
         var canvas = document.getElementById("canvas");
         var ctx = canvas.getContext("2d");
         ctx.beginPath();
-        ctx.arc(point.x, point.y, 3, 0, 2 * Math.PI);
+        ctx.arc(point.x, point.y, 1, 0, 2 * Math.PI);
         ctx.stroke();
         console.log(Point);
         sendPoint(Point);
     };
-    
+
     var sendPoint = function (pt) {
         console.log(pt);
-        stompClient.send("/topic/newpoint", {}, JSON.stringify({'x': parseInt($("#x").val()),'y': parseInt($("#y").val())}));
+        stompClient.send("/topic/newpoint", {}, JSON.stringify({'x': parseInt($("#x").val()), 'y': parseInt($("#y").val())}));
         //stompClient.send("/topic/newpoint", {}, JSON.stringify({'x': pt.x,'y': pt.y}));
     };
-    
+
     var getMousePosition = function (evt) {
         canvas = document.getElementById("canvas");
         var rect = canvas.getBoundingClientRect();
@@ -47,36 +47,41 @@ var app = (function () {
         console.info('Connecting to WS...');
         var socket = new SockJS('/stompendpoint');
         stompClient = Stomp.over(socket);
-        
+
         //subscribe to /topic/newpoint when connections succeed
         stompClient.connect({}, function (frame) {
             console.log('Connected: ' + frame);
             stompClient.subscribe('/topic/newpoint', function (eventbody) {
-                alert(eventbody);
+                //alert(eventbody);
                 console.log(eventbody);
                 var point = JSON.parse(eventbody.body);
-                console.log(point); 
-                var x = point.x;
-                var y = point.y;
+                console.log(point);
+                var canvas = document.getElementById("canvas");
+                var ctx = canvas.getContext("2d");
+                ctx.beginPath();
+                ctx.arc(point.x, point.y, 1, 0, 2 * Math.PI);
+                ctx.stroke();
+                //var x = point.x;
+                //var y = point.y;
             });
         });
 
     };
-    
-    
+
+
 
     return {
 
         init: function () {
             var can = document.getElementById("canvas");
-            
+
             //websocket connection
             connectAndSubscribe();
         },
 
-        publishPoint: function(px,py){
-            var pt=new Point(px,py);
-            console.info("publishing point at "+pt);
+        publishPoint: function (px, py) {
+            var pt = new Point(px, py);
+            console.info("publishing point at " + pt);
             addPointToCanvas(pt);
 
             //publicar el evento
