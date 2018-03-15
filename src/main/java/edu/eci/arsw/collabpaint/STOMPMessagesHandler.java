2e57f6f3dd5d8bb71f4b559970dbc2a4b36da83d
Fabian Ardila Rodriguez <fabianra97@gmail.com>
2018-03-15 04:37:20 -0500

    avance parte 4

---
+++ src/main/java/edu/eci/arsw/collabpaint/STOMPMessagesHandler.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.collabpaint;
+
+import edu.eci.arsw.collabpaint.model.Point;
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+/**
+ *
+ * @author Fabian Ardila
+ */
+@Controller
+public class STOMPMessagesHandler {
+
+    AtomicInteger numPoints = new AtomicInteger();
+    ArrayList<Point> points = new ArrayList<>();
+
+    @Autowired
+    SimpMessagingTemplate msgt;
+
+    @MessageMapping("/newpoint.{numdibujo}")
+    public void handlePointEvent(Point pt, @DestinationVariable String numdibujo) throws Exception {
+        points.add(pt);
+        System.out.println("Nuevo punto recibido en el servidor!:" + pt);
+        msgt.convertAndSend("/topic/newpoint." + numdibujo, pt);
+        synchronized (this) {
+            if (numPoints.incrementAndGet() >= 3) {
+                System.err.println("Send new points!");
+                msgt.convertAndSend("/topic/newpolygon." + numdibujo, points);
+                points.clear();
+                numPoints.set(0);
+            }
+        }
+    }
+}
--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -4,14 +4,23 @@ var app = (function () {
         constructor(x, y) {
             this.x = x;
             this.y = y;
-        } 
-
+            this.angle = 0;
+        }
+        
+        setAngle(a) {
+            this.angle = a;
+        }
+        
         x() {
             return this.x;
         }
 
         y() {
             return this.y;
+        }
+        
+        getAngle() {
+            return this.angle;
         }
     }
 
@@ -24,13 +33,13 @@ var app = (function () {
         ctx.beginPath();
         ctx.arc(point.x, point.y, 1, 0, 2 * Math.PI);
         ctx.stroke();
-        console.log(Point);
+        //console.log(Point);
         sendPoint(Point);
     };
 
     var sendPoint = function (pt) {
-        console.log(pt);
-        stompClient.send("/topic/newpoint." + $("#drawId"), {}, JSON.stringify({'x': parseInt($("#x").val()), 'y': parseInt($("#y").val())}));
+        //console.log(pt);
+        stompClient.send("/app/newpoint." + $("#drawId"), {}, JSON.stringify({'x': parseInt($("#x").val()), 'y': parseInt($("#y").val())}));
         //stompClient.send("/topic/newpoint", {}, JSON.stringify({'x': pt.x,'y': pt.y}));
     };
 
@@ -65,22 +74,105 @@ var app = (function () {
                 //var x = point.x;
                 //var y = point.y;
             });
+            stompClient.subscribe('/topic/newpolygon.' + $("#drawId"), function (eventbody) {
+                console.log("Poligono:");
+                console.log(eventbody);
+                drawPolygon(eventbody.body);
+            });
         });
 
     };
 
+    var drawPolygon = function (points) {
+        console.log("angulo:");
+            console.log(points[0].angle);
+        
+        var canvas = document.getElementById('canvas');
+        if (canvas.getContext) {
+            var ctx = canvas.getContext('2d');
+            ctx.fillStyle = "red";
+            console.log("Configuro el canvas.");
 
+            // calculate max and min x and y
+            var minX = points[0].x;
+            var maxX = points[0].x;
+            var minY = points[0].y;
+            var maxY = points[0].y;            
+
+            for (var i = 1; i < points.length; i++) {
+                if (points[i].x < minX) minX = points[i].x;
+                if (points[i].x > maxX) maxX = points[i].x;
+                if (points[i].y < minY) minY = points[i].y;
+                if (points[i].y > maxY) maxY = points[i].y;
+            }
+            console.log("Calculo el max y el min");
+
+            // choose a "central" point
+            var center = {
+                x: minX + (maxX - minX) / 2,
+                y: minY + (maxY - minY) / 2
+            };
+            console.log("Escogio el punto central");
+
+            // precalculate the angles of each point to avoid multiple calculations on sort
+            for (var i = 0; i < points.length; i++) {
+                points[i].angle(Math.acos((points[i].x - center.x) / lineDistance(center, points[i])));
+
+                if (points[i].y > center.y) {
+                    points[i].angle(Math.PI + Math.PI - points[i].angle);
+                }
+            }
+            console.log("Precalculo los angulos");
+            
+            console.log(points);
+            
+            // sort by angle
+            points.sort(function(a, b) {return a.getAngle() - b.getAngle();});
+
+            // Draw
+            console.log("Comienza a dibujar");
+            ctx.beginPath();
+            ctx.moveTo(points[0].x, points[0].y);
+
+            for (var i = 1; i < points.length; i++) {
+                ctx.lineTo(points[i].x, points[i].y);
+            }
+
+            ctx.lineTo(points[0].x, points[0].y);
+
+            ctx.stroke();
+            ctx.fill();
+        }
+    };
+    
+    var funcSort = function (pts) {
+        pts.sort(function(a, b){return a.angle-b.angle});
+        return pts;
+    };
+
+    var lineDistance = function (point1, point2) {
+        var xs = 0;
+        var ys = 0;
+
+        xs = point2.x - point1.x;
+        xs = xs * xs;
+
+        ys = point2.y - point1.y;
+        ys = ys * ys;
+
+        return Math.sqrt(xs + ys);
+    };
 
     return {
 
         /*init: function () {
-            var can = document.getElementById("canvas");
+         var can = document.getElementById("canvas");
+         
+         //websocket connection
+         connectAndSubscribe();
+         },*/
 
-            //websocket connection
-            connectAndSubscribe();
-        },*/
-        
-        connect : function () {
+        connect: function () {
             connectAndSubscribe();
         },
 
